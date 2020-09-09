023df7fb45b9d6e51dade615912b9a948b397052
JoaoFula <hsifula@gmail.com>
2020-09-09 11:30:15 +0100

    REST API fully functional

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse, abort
-
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Video(name = {name}, views = {views}, likes = {likes})"
+
+# db.create_all() Run this only once
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
 video_put_args.add_argument("views", type=int, help="Views of the video are required", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes of the video are required", required=True)
 
+video_update_args =  reqparse.RequestParser()
+video_update_args.add_argument("name", type=str, help="Name of the video is required")
+video_update_args.add_argument("views", type=int, help="Views of the video are required")
+video_update_args.add_argument("likes", type=int, help="Likes of the video are required")
+
+'''   This was some piece of code that is no longer in use
 videos = {}
 
 def abort_if_video_id_doesnt_exist(video_id):
@@ -19,22 +38,52 @@ def abort_if_video_id_doesnt_exist(video_id):
 def abort_if_video_exists(video_id):
     if video_id in videos:
         abort(409, message="Video already exists with that ID")
+'''
+
+resource_fields={
+    'id': fields.Integer,
+    'name': fields.String,
+    'views': fields.Integer,
+    'likes': fields.Integer,
+}
 
 class Video(Resource):
+    @marshal_with(resource_fields) #serialize returns into the json format that can be returned
     def get(self, video_id):
-        abort_if_video_id_doesnt_exist(video_id)
-        return videos[video_id]
+        result = VideoModel.query.filter_by(id=video_id).first() # This will store the first VideoModel that has id=video_id
+        if not result:
+            abort(404, message="Could not find video with that ID")
 
+        return result
+
+    @marshal_with(resource_fields)
     def put(self, video_id):
-        abort_if_video_exists(video_id)
         args = video_put_args.parse_args()
-        videos[video_id] = args
-        return videos[video_id], 201
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if result:
+            abort(409, message="Video ID already exists")
 
-    def delete(self, video_id):
-        abort_if_video_id_doesnt_exist(video_id)
-        del videos[video_id]
-        return '', 204
+        video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
+
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Could not find video with that ID, cannot update")
+        if "name" in args and args["name"] is not None:
+            result.name = args["name"]
+        if "likes" in args and args["likes"] is not None:
+            result.likes = args["likes"]
+        if "views" in args and args["views"] is not None:
+            result.views = args["views"]
+
+        db.session.commit()
+
+        return result
 
 api.add_resource(Video, "/video/<int:video_id>")
 
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 
 BASE_URL = "http://127.0.0.1:5000/"
-
+'''
 data = [{"likes": 10, "name":"Tim", "views": 100},
         {"likes": 210, "name":"Joe", "views": 10220},
         {"likes": 34, "name":"Brian", "views": 1003},
@@ -9,10 +9,14 @@ data = [{"likes": 10, "name":"Tim", "views": 100},
         {"likes": 12424, "name":"How to make REST APIs", "views": 1051160}]
 
 for i in range(len(data)):
-    response = requests.put(BASE_URL + str(i), data[i])
+    response = requests.put(BASE_URL + "video/" + str(i), data[i])
     print(response.json())
 
-response=requests.delete(BASE_URL + "video/0")
+
 input()
-response = requests.get(BASE_URL + 'video/2')
+response = requests.get(BASE_URL + 'video/6')
+print(response.json())
+'''
+
+response = requests.patch(BASE_URL+"video/2", {"views": 99, "likes": 1})
 print(response.json())
